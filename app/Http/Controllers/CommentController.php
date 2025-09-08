91652e9145483457ce948824c8c16a1e7679da45
ichinosetakayuki <ichinosetakayuki92@gmail.com>
2025-09-08 22:44:33 +0900

    add CommentController show.blade routing etc

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Diary;
+use App\Models\Comment;
+
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request, Diary $diary)
+    {
+        if(!$diary->is_public && auth()->id() !== $diary->user_id) {
+            return abort(403);
+        }
+
+        $validated = $request->validate([
+            'body' => 'required|string|max:2000'
+        ]);
+
+        $diary->comments()->create([
+            'user_id' => auth()->id(),
+            'body' => $validated['body'],
+        ]);
+
+        return back()->with('status', 'コメントを投稿しました。');
+    }
+
+    public function destroy(Comment $comment)
+    {
+        $comment->delete();
+
+        return back()->with('status', 'コメントを削除しました。');
+    }
 }
--- resources/views/diaries/show.blade.php
+++ resources/views/diaries/show.blade.php
@@ -48,7 +48,37 @@
         </div>
 
         {{-- コメント部分 --}}
-        <h3 class="text-lg font-semibold mb-2">コメント({{ $diary->comments->count() }})</h3>
+        <h3 class="text-lg font-semibold my-2">⭐️コメント({{ $diary->comments->count() }})</h3>
+
+        {{-- コメント入力 →モーダルへ --}}
+        <form method="post" action="{{ route('comments.store', $diary) }}" class="mb-6">
+            @csrf
+            <x-textarea name="body" rows="3" placeholder="コメントを書く・・・" />
+            <x-input-error :messages="$errors->get('body')" />
+            <x-primary-button>送信</x-primary-button>
+        </form>
+
+        {{-- コメント一覧 --}}
+        <ul class="space-y-4">
+            @forelse($diary->comments as $comment)
+            <li>
+                <div>
+                    <span class="text-sm font-semibold">{{ $comment->user->name ?? '退会ユーザー' }}</span>
+                    <span class="text-xs ml-1">{{ $comment->updated_at->diffForHumans() }}</span>
+                </div>
+                <p class="whitespace-pre-wrap bg-brand-light shadow-md rounded-lg p-4 text-sm">{{ $comment->body }}</p>
+                @if( auth()->id() === $comment->user_id )
+                <form method="post" action="{{ route('comments.destroy', $comment)} }">
+                    @csrf
+                    @method('DELETE')
+                    <x-secondary-button onclick="return confirm('このコメントを削除しますか？')">削除</x-secondary-button>
+                </form>
+                @endif
+            </li>
+            @empty
+            <li class="text-sm text-gray-500">まだコメントはありません</li>
+            @endforelse
+        </ul>
 
 
     </div>
