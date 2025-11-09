9df3b05c5584edf19db95f3927e4195d4118070b
ichinosetakayuki <ichinosetakayuki92@gmail.com>
2025-11-09 17:00:30 +0900

    不要なコメントアウトを削除し、日記モデルのlikersメソッドを整理

--- app/Models/Diary.php
+++ app/Models/Diary.php
@@ -57,10 +57,6 @@ class Diary extends Model
         return $this->likes()->where('user_id', $user->id)->exists();
     }
 
-    // public function likers()
-    // {
-    //     return $this->belongsToMany(User::class, 'likes')->withTimestamps();
-    // }
     public function likers()
     {
         return $this->morphToMany(User::class,  'likeable', 'likes', 'likeable_id', 'user_id')
